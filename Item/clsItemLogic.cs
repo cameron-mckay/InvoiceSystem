ae28f2d674e9950ad5211c93114dad28fa5c8093
calebm01 <42648514+calebm01@users.noreply.github.com>
2023-11-14 17:13:44 -0700

    update logic class with comments describing what will be added down the line

--- Item/clsItemLogic.cs
+++ Item/clsItemLogic.cs
@@ -8,5 +8,47 @@ namespace InvoiceSystem.Item
 {
     public class clsItemLogic
     {
+            
+    /*
+    a separate class, clsItem will be used that just holds:
+
+    code  
+
+    cost
+
+    description
+       
+
+    
+    public static List GetAllItems()
+    {
+
+        return List<clsItem>;
+
+        this method will get all of our items in the database as a list
+    }
+
+    public void AddItem(clsItem)
+    {
+        this method will add an item, needing a type of clsItem to be passed in
+    }
+
+    public void EditItem(clsItem clsOldItem, clsItem clsNewItem)
+    {
+        this method will edit an item, needing two clsItem types to be passed in, one for the old item and one for the old item
+    }
+
+    public void DeleteItem(clsItem clsItemtoDelete)
+    {
+        this method will delete  an item, getting passed in the clsItem that needs to be deleted
+    }
+
+    public bool IsItemOnInvoice(clsItem)
+    {
+        this method will check if a given item is on an invoice, if true, the item cannot be deleted
+        return false;
+    } */
+        
+    }
     }
 }
