d7c2f75033f2d0117cedefa9465108f19cc622d1
Cameron McKay <cameronmckay62@gmail.com>
2023-11-30 20:31:04 -0700

    Update clsMainLogic.cs

--- Main/clsMainLogic.cs
+++ Main/clsMainLogic.cs
@@ -2,23 +2,133 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace InvoiceSystem
 {
+    /// <summary>
+    /// Logic for the main window
+    /// </summary>
     public class clsMainLogic
     {
-        public static clsInvoice getInvoiceByID(int invoiceID)
+        /// <summary>
+        /// Database connection
+        /// </summary>
+        clsDataAccess db;
+        /// <summary>
+        /// Constructor for main logic
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        public clsMainLogic()
         {
-            // Get line items from db
+            try
+            {
+                db = new clsDataAccess();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + 
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + e.Message);
+            }
+        }
+        /// <summary>
+        /// Fetches an invoice object by ID
+        /// </summary>
+        /// <param name="invoiceID"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public clsInvoice getInvoiceByID(int invoiceID)
+        {
 
-            // Create the list of items
+            try
+            {
+                // Get line items from db
 
-            // Get the invoice from the db
+                // Create the list of items
 
-            // Get
-            return new clsInvoice();
+                // Get the invoice from the db
+
+                // Create the invoice object and return
+                return new clsInvoice();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + 
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + e.Message);
+            }
+        }
+        /// <summary>
+        /// Creates a new invoice in the database and returns the ID
+        /// </summary>
+        /// <param name="invoice"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public int createNewInvoice(clsInvoice invoice)
+        {
+            try
+            {
+                // Get line items from db
+
+                // Create the list of items
+
+                // Get the invoice from the db
+
+                // Create the invoice object and return
+                return 1;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + 
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + e.Message);
+            }
+        }
+        /// <summary>
+        /// Updates an invoice
+        /// </summary>
+        /// <param name="invoice"></param>
+        /// <exception cref="Exception"></exception>
+        public void updateInvoice(clsInvoice invoice)
+        {
+            try
+            {
+                // Delete existing line items
+
+                // update invoice
+
+                // create new line items
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + 
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + e.Message);
+            }
+        }
+        /// <summary>
+        /// Returns true if an invoice is valid
+        /// </summary>
+        /// <param name="invoice"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public bool invoiceValid(clsInvoice invoice)
+        {
+            try
+            {
+                // Delete existing line items
+
+                // update invoice
+
+                // create new line items
+                
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + 
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + e.Message);
+            }
         }
     }
 }
